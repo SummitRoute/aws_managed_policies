9bbbbfbdf00259e38db7fd0f5038c9349a0f1a34
Victor GRENU <victor.grenu@gmail.com>
2021-04-13 21:15:51 +0200

    better echos

--- automation/runbook-dev.sh
+++ automation/runbook-dev.sh
@@ -27,21 +27,19 @@ then
     # push the changes if any
     if [[ -n $(git status -s) ]];
     then
-        echo "==> Tagging"
-
-        echo "==> Push the changes to master"
         # Prepare the Tweet
         diff="$(git diff --name-only) $(git ls-files --others --exclude-standard)"
         diff=${diff//$WORDTOREMOVE/}
         diff="${diff:0:200}..."
         git add ./policies
         git commit -am "Update detected"
+        echo "==> Tagging"
         git tag $DATE
         # Craft commit ID for tweet direct URL
         commit_id=$(git log --format="%h" -n 1)
         # Send message to qTweeter for publishing the tweet
         echo "aws sqs send-message --queue-url https://sqs.eu-west-1.amazonaws.com/567589703415/qtweet-mamip-sqs-queue.fifo --message-body "$diff https://github.com/z0ph/MAMIP/commit/$commit_id" --message-group-id 1"
-        # push to mamip repository
+        echo "==> Push the changes to dev"
         git push origin dev --tags
     else
         echo "==> No changes detected"
